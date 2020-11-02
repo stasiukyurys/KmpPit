6ca80fa20e596d08f2cad2967262975d541afadb
stasiukyurys@gmail.com <stasiukyurys@gmail.com>
2020-11-02 21:14:39 +0300

    Grid вместо List

--- KmpPit/PagePit.xaml.cs
+++ KmpPit/PagePit.xaml.cs
@@ -1,9 +1,35 @@
-﻿using System.Data.OleDb;
+﻿using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace KmpPit
 {
+    class MyTable
+    {
+        public MyTable(string Data, 
+                       string Time, 
+                       string Id, 
+                       string WeightAuto, 
+                       string Brutto,
+                       string Netto
+                       )
+        {
+            this.Data = Data;
+            this.Time = Time;
+            this.Id = Id;
+            this.WeightAuto = WeightAuto;
+            this.Brutto = Brutto;
+            this.Netto = Netto;
+        }
+        public string Data { get; set; }
+        public string Time { get; set; }
+        public string Id { get; set; }
+        public string WeightAuto { get; set; }
+        public string Brutto { get; set; }
+        public string Netto { get; set; }
+
+    }
     public partial class PagePit : Page
     {
         // вариант 1
@@ -27,27 +53,32 @@ namespace KmpPit
             // очищаем listBox1
             PitListBox.Items.Clear();
             // в цикле построчно читаем ответ от БД
+            List<MyTable> result = new List<MyTable>(3);
             while (reader.Read())
             {
                 // выводим данные столбцов текущей строки в listBox1
-                PitListBox.Items.Add(
-                      reader[0].ToString() + "      " 
-                    + reader[1].ToString() + "      " 
-                    + reader[2].ToString() + "      "
-                    + reader[3].ToString() + "      "
-                    + reader[4].ToString() + "      "
-                    + reader[5].ToString() + "      "
-                                    );
+                result.Add(new MyTable(reader[0].ToString(),
+                                       reader[1].ToString(),
+                                       reader[2].ToString(),
+                                       reader[3].ToString(),
+                                       reader[4].ToString(),
+                                       reader[5].ToString()
+                           ));
             }
             // закрываем OleDbDataReader
             reader.Close();
-
+            grid.ItemsSource = result;
         }
 
         private void ButtonClose_Click(object sender, RoutedEventArgs e)
         {
             myConnection.Close();
             this.NavigationService.Navigate(null);
+        }
+
+        private void grid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
